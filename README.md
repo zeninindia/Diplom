5eabd87ced3b0f12adc5026342a30dd68d07098b
Olga Zenkina <zeninindia@gmail.com>
2026-04-05 04:08:57 +0300

    README++++

--- README.md
+++ README.md
@@ -13,6 +13,7 @@
 
 * pip install pytest sylenium
 * pip install pytest
+* *pip install webdriver-manager*
 * pip install selenium webdriver-manager
 * pip install requests pytest pytest-html pytest-cov responses python-dotenv loguru allure-pytest
 * python -m venv test_env
@@ -33,3 +34,4 @@
 
 * Это про **маркдаун**, и как их использовать [Markdown](https://www.markdownguide.org/basic-syntax/)
 * Нужно выбрать те функции с которыми работаем и вот [генератор файла **.gitignore**](https://www.toptal.com/developers/gitignore/api/python,windows,pycharm,visualstudiocode,test)
+* Это [API](https://api.poiskkino.dev) документация сайта **Кинопоиск**
--- UI/test_ui.py
+++ UI/test_ui.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.expected_conditions import element_to_be_clickable
 from selenium.webdriver.support.ui import WebDriverWait
 import allure
 import selenium
@@ -8,10 +9,48 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def browser():
-    driver = selenium.webdriver.Chrome()
-    driver.maximize_window()
-    return driver
+# def browser(driver):
+#     driver = selenium.webdriver.Chrome()
+#     driver.maximize_window()
+#     return
 
-def test_ui_1(browser):
-    browser = selenium.webdriver.Chrome()
+
+@pytest.fixture
+def driver():
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(30)
+    browser.maximize_window()
+    yield browser
+    browser.quit()
+# class="CheckboxCaptcha-Button"
+
+
+def test_ui_1(driver):
+    driver.get('https://www.kinopoisk.ru/?utm_referrer=organic.kinopoisk.ru')
+    wait = WebDriverWait(driver, 5)
+    try:
+        # Ищем кнопку капчи по CSS‑селектору
+        captcha_button = wait.until(
+            EC.element_to_be_clickable((By.CSS_SELECTOR, ".CheckboxCaptcha-Button"))
+        )
+        captcha_button.click()
+    except:
+        pass
+    # driver.find_element(By.XPATH, "(//a[contains(text(),'Онлайн-кинотеатр')])[1]").click()
+    # нажать на кнопку 'Онлайн-кинотеатр' XPATH (//a[contains(text(),'Онлайн-кинотеатр')])[1]
+    online_cinema_btn = wait.until(
+        EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Онлайн-кинотеатр')]"))
+    )
+    online_cinema_btn.click()
+    # нажать на кнопку смотреть бесплатно CSS_SELECTOR data-test-id="SubscriptionPurchaseButton"
+    button_free = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[data-test-id="SubscriptionPurchaseButton"]'))
+    )
+    button_free.click()
+    # нажать на кнопку узнать больше CSS_SELECTOR class="ya_5197c563 ya_7e5621e5 AuthPromo-link"
+
+
+    #driver.get('https://www.kinopoisk.ru/')
+
+# https://www.kinopoisk.ru/?utm_referrer=organic.kinopoisk.ru
+# https://api.poiskkino.dev это API документация кинопоиска
+# "styles_iconHover__vNlVU styles_icon__lTtDI"
