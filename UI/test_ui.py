a5bdfbff14996c15c58bedfec17f06ff4b69f27c
Olga Zenkina <zeninindia@gmail.com>
2026-04-07 21:47:24 +0300

    new place commit

--- UI/test_ui.py
+++ UI/test_ui.py
@@ -7,13 +7,13 @@ from selenium import webdriver
 from time import sleep
 
 
-@pytest.fixture
-def driver():
-    browser = webdriver.Chrome()
-    browser.implicitly_wait(30)
-    browser.maximize_window()
-    yield browser
-    browser.quit()
+# @pytest.fixture
+# def driver():
+#     browser = webdriver.Chrome()
+#     browser.implicitly_wait(30)
+#     browser.maximize_window()
+#     yield browser
+#     browser.quit()
 
 
 
@@ -32,6 +32,10 @@ def test_buttons_free(driver):
     # нажать на кнопку 'Онлайн-кинотеатр' XPATH (//a[contains(text(),'Онлайн-кинотеатр')])[1]
     online_cinema_btn = wait.until(
         EC.element_to_be_clickable((By.XPATH, "//a[contains(text(), 'Онлайн-кинотеатр')]"))
+
+    # assert что при нажатии на кнопку онлайн кинотеатр открывается страница с url = https://hd.kinopoisk.ru/
+    # assert что на странице есть сообщение "30 дней бесплатно"
+    # assert что на кнопке написано "Попробовать бесплатно"
     )
     online_cinema_btn.click()
     sleep(2)
@@ -40,11 +44,18 @@ def test_buttons_free(driver):
     )
     button_free.click()
     sleep(2)
+
+    # assert  что нажатие на кнопку "попробовать бесплатно" переходит на сайт вот с таким url = https://passport.yandex.ru/pwl-yandex/auth/add?origin=kinopoisk&retpath=https%3A%2F%2Fsso.passport.yandex.ru%2Fpush%3Fretpath%3Dhttps%253A%252F%252Fhd.kinopoisk.ru%252F%26uuid%3D6163884d-4f5f-4936-8bea-a7e21a45aa0a&cause=auth&process_uuid=50f01302-002b-4f21-94b2-2491bd976d9b
+    # assert что есть сообщение на странице "Яндекс ID — ключ для всех сервисов"
+
     # нажать на кнопку узнать больше CSS_SELECTOR class="ya_5197c563 ya_7e5621e5 AuthPromo-link"
     btn_know_more = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, '[class="ya_5197c563 ya_7e5621e5 AuthPromo-link"]'))
     )
     driver.execute_script("arguments[0].scrollIntoView(true);", btn_know_more)
     btn_know_more.click()
+    # assert что нажатие на кнопку "узнать больше" открывается страница с url = https://yandex.ru/id/about
+    # assert что на странице есть плашка, кнопка "в приложении - удобнее"
+
     sleep(2)
 
 def test_get_trailer_to_buy(driver):
@@ -76,6 +87,12 @@ def test_get_trailer_to_buy(driver):
     choose_trailer.click()
     sleep(2)
 
+    # assert что есть кнопка на которой написано "Смотреть кино бесплатно"
+    # assert что на странице фильма на постере написано
+    #    описание "class="ContentOverview_description__CXd5E" c текстом....
+    # assert
+    # assert
+
 def test_get_top_soap(driver):
     driver.get('https://www.kinopoisk.ru/')
     wait = WebDriverWait(driver, 10)
@@ -106,8 +123,16 @@ def test_get_top_soap(driver):
     choose_online.click()
     sleep(2)
 
+    # assert
+    # assert
+    # assert
+    # assert
+    # assert
+
+
 
 def test_correct_data(driver):
 
     driver.get('https://www.kinopoisk.ru/')
     sleep(5)
+
